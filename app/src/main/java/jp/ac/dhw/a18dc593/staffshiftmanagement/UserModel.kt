07df181320be2a6afd90db3a877e24a988306b45
masakiando2003 <masakiando2003@gmail.com>
2020-01-28 10:10:24 +0900

    2020-01-28
    UserRegisterActivity Finished
    
    Signed-off-by: masakiando2003 <masakiando2003@gmail.com>

---
+++ app/src/main/java/jp/ac/dhw/a18dc593/staffshiftmanagement/UserModel.kt
@@ -0,0 +1,11 @@
+package jp.ac.dhw.a18dc593.staffshiftmanagement
+
+import com.google.firebase.database.IgnoreExtraProperties
+
+@IgnoreExtraProperties
+data class UserModel(
+    val name: String?,
+    val email: String?,
+    val password: String?,
+    val role: String?
+)
