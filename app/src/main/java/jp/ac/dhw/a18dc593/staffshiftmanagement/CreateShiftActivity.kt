14448afe43ef938900269ec6abf26e5bb6b30019
masakiando2003 <masakiando2003@gmail.com>
2020-01-28 11:56:16 +0900

    2020-01-28
    CreateShiftActivity: ~80%
    Signed-off-by: masakiando2003 <masakiando2003@gmail.com>

--- app/src/main/java/jp/ac/dhw/a18dc593/staffshiftmanagement/CreateShiftActivity.kt
+++ app/src/main/java/jp/ac/dhw/a18dc593/staffshiftmanagement/CreateShiftActivity.kt
@@ -1,20 +1,34 @@
 package jp.ac.dhw.a18dc593.staffshiftmanagement
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.text.TextUtils
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class CreateShiftActivity : AppCompatActivity() {
+
+    private lateinit var databaseReference: DatabaseReference
+
+    private val myPREFERENCES = "MyPrefs"
+    var sharedpreferences: SharedPreferences? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.shift_edit)
 
+        sharedpreferences = getSharedPreferences(myPREFERENCES, Context.MODE_PRIVATE)
 
+        if(sharedpreferences!!.contains("loginUserName")){
+            var loginUserStr = findViewById<TextView>(R.id.txtUserName)
+            loginUserStr.text = sharedpreferences!!.getString("loginUserName",null)
+        }
 
         val btnShiftEditBack = findViewById<Button>(R.id.btnShiftEditBack)
 
@@ -23,9 +37,9 @@ class CreateShiftActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
-        val btnSubmit = findViewById<Button>(R.id.btnSubmit)
+        val btnShiftEditSubmit = findViewById<Button>(R.id.btnShiftEditSubmit)
 
-        btnSubmit.setOnClickListener {
+        btnShiftEditSubmit.setOnClickListener {
             var submitFlag = false
 
             val userName = findViewById<TextView>(R.id.txtUserName).text.toString()
@@ -48,8 +62,16 @@ class CreateShiftActivity : AppCompatActivity() {
             }
 
             if(submitFlag){
+                Toast.makeText(baseContext, "シフトを登録しています。少々お待ちください...",
+                    Toast.LENGTH_SHORT).show()
+                databaseReference = FirebaseDatabase.getInstance().reference
 
-
+                val shiftData = ShiftModel(attendTime, endTime, memo)
+                var shiftDateArr = attendDate.split("/")
+                val shiftDate = shiftDateArr.joinToString("")
+                databaseReference.child("shift_list").child(shiftDate).child(userName).setValue(shiftData)
+                Toast.makeText(baseContext, "シフトを登録しました。",
+                    Toast.LENGTH_SHORT).show()
                 val intent = Intent(this, MainActivity::class.java)
                 startActivity(intent)
             }
